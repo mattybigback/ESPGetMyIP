2ecb84571e2f99a0c023f30b3d2ed640e1807c61
Matt Harrison <mattybigback@gmail.com>
2024-01-25 19:15:16 +0000

    Changed default screen cycle time

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POS
 WiFiManager wm;
 const uint8_t bootIP[] = {192, 168, 100, 1};
 uint8_t screenNo = 0;
-uint16_t screenChangeInterval = 1000;
+uint16_t screenChangeInterval = 5000;
 uint32_t timePast = 0;
 char publicIP[16] = "0.0.0.0"; // 16 bytes for "xxx.xxx.xxx.xxx\0"
 
